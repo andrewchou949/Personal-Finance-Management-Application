00a83d3574ab95b978c56c5b431ffc3a83b9595a
Andrew <andrewchou949@gmail.com>
2024-06-21 19:50:28 -0700

    Added a link to navigate to transaction list page from transaction page

--- frontend/src/components/Transactions/Transactions.js
+++ frontend/src/components/Transactions/Transactions.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Transactions.css';
 
@@ -120,6 +121,7 @@ const Transactions = () => {
                         </li>
                     ))}
                 </ul>
+                <Link to="/transaction-list" className="transaction-list-link">View All Transactions</Link>
             </div>
         </div>
     );
