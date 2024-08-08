6f00efffe2b111cd5aa3c1b4b2f7a27d4fa886af
Andrew <andrewchou949@gmail.com>
2024-08-07 17:30:00 -0700

    Added classification_report and accuracy_score to python script

--- fraud_detection/fraud_detection.py
+++ fraud_detection/fraud_detection.py
@@ -3,6 +3,7 @@ import pandas as pd # for data preprocessing and analysis
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
 import joblib # saving the training to a result (.pkl)
 
 # Load the dataset
